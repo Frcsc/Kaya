95d4615a1903d664dc7599162cbefa39ba7b1529
Sedia <info@Frcsc.realtek>
2024-11-06 20:24:40 +0800

    chore: add two new django apps for ad_group and ad_group_stats

---
+++ ad_group_stats/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdGroupStatsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ad_group_stats"
--- kaya/settings.py
+++ kaya/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django_filters",
     "import_export",
     "campaign",
+    "ad_group",
+    "ad_group_stats",
 ]
 
 MIDDLEWARE = [
